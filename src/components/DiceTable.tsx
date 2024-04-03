7b16b1b2dd2f61789119bb21186971edb96f7de4
Feifei Sun <feifeisun0503@gmail.com>
2024-04-02 22:08:38 -0400

    fix dice table

--- src/components/DiceTable.tsx
+++ src/components/DiceTable.tsx
@@ -1,9 +1,9 @@
 const DiceTable = () => {
     return (
-        <div className="relative overflow-x-auto max-w-md min-w-96 p-4">
-            <table className=" font-mono w-full text-sm text-left rtl:text-right ">
+        <div className="relative overflow-x-auto max-w-2xl min-w-96 p-4">
+            <table className=" font-mono w-full text-sm text-center">
                 <thead className="text-xs  uppercase">
-                    <th scope="col">Value</th>
+                    <th scope="col"></th>
                     <th scope="col">1 Dice</th>
                     <th scope="col">2 Dice</th>
                     <th scope="col">3 Dice</th>
@@ -14,191 +14,191 @@ const DiceTable = () => {
                     <th scope="col">8 Dice</th>
                 </thead>
                 <tr>
-                    <th scope="row">{">"}=0</th>
-                    <td>100%</td>
-                    <td>100%</td>
-                    <td>100%</td>
-                    <td>100%</td>
-                    <td>100%</td>
-                    <td>100%</td>
-                    <td>100%</td>
-                    <td>100%</td>
+                    <th scope="row">{"2"}=0</th>
+                    <td className="px-2 py-1">100%</td>
+                    <td className="px-2 py-1">100%</td>
+                    <td className="px-2 py-1">100%</td>
+                    <td className="px-2 py-1">100%</td>
+                    <td className="px-2 py-1">100%</td>
+                    <td className="px-2 py-1">100%</td>
+                    <td className="px-2 py-1">100%</td>
+                    <td className="px-2 py-1">100%</td>
                 </tr>
                 <tr>
-                    <th scope="row">{">"}=1</th>
-                    <td>67%</td>
-                    <td>89%</td>
-                    <td>96%</td>
-                    <td>99%</td>
-                    <td>100%</td>
-                    <td>100%</td>
-                    <td>100%</td>
-                    <td>100%</td>
+                    <th scope="row">{"2"}=1</th>
+                    <td className="px-2 py-1">67%</td>
+                    <td className="px-2 py-1">89%</td>
+                    <td className="px-2 py-1">96%</td>
+                    <td className="px-2 py-1">99%</td>
+                    <td className="px-2 py-1">100%</td>
+                    <td className="px-2 py-1">100%</td>
+                    <td className="px-2 py-1">100%</td>
+                    <td className="px-2 py-1">100%</td>
                 </tr>
                 <tr>
-                    <th scope="row">{">"}=2</th>
-                    <td>33%</td>
-                    <td>67%</td>
-                    <td>85%</td>
-                    <td>94%</td>
-                    <td>98%</td>
-                    <td>99%</td>
-                    <td>100%</td>
-                    <td>100%</td>
+                    <th scope="row">{"2"}=2</th>
+                    <td className="px-2 py-1">33%</td>
+                    <td className="px-2 py-1">67%</td>
+                    <td className="px-2 py-1">85%</td>
+                    <td className="px-2 py-1">94%</td>
+                    <td className="px-2 py-1">98%</td>
+                    <td className="px-2 py-1">99%</td>
+                    <td className="px-2 py-1">100%</td>
+                    <td className="px-2 py-1">100%</td>
                 </tr>
                 <tr>
-                    <th scope="row">{">"}=3</th>
-                    <td></td>
-                    <td>33%</td>
-                    <td>63%</td>
-                    <td>81%</td>
-                    <td>91%</td>
-                    <td>96%</td>
-                    <td>98%</td>
-                    <td>99%</td>
+                    <th scope="row">{"2"}=3</th>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1">33%</td>
+                    <td className="px-2 py-1">63%</td>
+                    <td className="px-2 py-1">81%</td>
+                    <td className="px-2 py-1">91%</td>
+                    <td className="px-2 py-1">96%</td>
+                    <td className="px-2 py-1">98%</td>
+                    <td className="px-2 py-1">99%</td>
                 </tr>
                 <tr>
-                    <th scope="row">{">"}=4</th>
-                    <td></td>
-                    <td>11%</td>
-                    <td>37%</td>
-                    <td>62%</td>
-                    <td>79%</td>
-                    <td>89%</td>
-                    <td>95%</td>
-                    <td>98%</td>
+                    <th scope="row">{"2"}=4</th>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1">11%</td>
+                    <td className="px-2 py-1">37%</td>
+                    <td className="px-2 py-1">62%</td>
+                    <td className="px-2 py-1">79%</td>
+                    <td className="px-2 py-1">89%</td>
+                    <td className="px-2 py-1">95%</td>
+                    <td className="px-2 py-1">98%</td>
                 </tr>
                 <tr>
-                    <th scope="row">{">"}=5</th>
-                    <td></td>
-                    <td></td>
-                    <td>15%</td>
-                    <td>38%</td>
-                    <td>60%</td>
-                    <td>77%</td>
-                    <td>87%</td>
-                    <td>94%</td>
+                    <th scope="row">{"2"}=5</th>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1">15%</td>
+                    <td className="px-2 py-1">38%</td>
+                    <td className="px-2 py-1">60%</td>
+                    <td className="px-2 py-1">77%</td>
+                    <td className="px-2 py-1">87%</td>
+                    <td className="px-2 py-1">94%</td>
                 </tr>
                 <tr>
-                    <th scope="row">{">"}=6</th>
-                    <td></td>
-                    <td></td>
-                    <td>3.7%</td>
-                    <td>19%</td>
-                    <td>40%</td>
-                    <td>60%</td>
-                    <td>75%</td>
-                    <td>86%</td>
+                    <th scope="row">{"2"}=6</th>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1">3.7%</td>
+                    <td className="px-2 py-1">19%</td>
+                    <td className="px-2 py-1">40%</td>
+                    <td className="px-2 py-1">60%</td>
+                    <td className="px-2 py-1">75%</td>
+                    <td className="px-2 py-1">86%</td>
                 </tr>
                 <tr>
-                    <th scope="row">{">"}=7</th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td>6.2%</td>
-                    <td>21%</td>
-                    <td>40%</td>
-                    <td>59%</td>
-                    <td>74%</td>
+                    <th scope="row">{"2"}=7</th>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1">6.2%</td>
+                    <td className="px-2 py-1">21%</td>
+                    <td className="px-2 py-1">40%</td>
+                    <td className="px-2 py-1">59%</td>
+                    <td className="px-2 py-1">74%</td>
                 </tr>
                 <tr>
-                    <th scope="row">{">"}=8</th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td>1.2%</td>
-                    <td>8.6%</td>
-                    <td>23%</td>
-                    <td>41%</td>
-                    <td>58%</td>
+                    <th scope="row">{"2"}=8</th>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1">1.2%</td>
+                    <td className="px-2 py-1">8.6%</td>
+                    <td className="px-2 py-1">23%</td>
+                    <td className="px-2 py-1">41%</td>
+                    <td className="px-2 py-1">58%</td>
                 </tr>
                 <tr>
-                    <th scope="row">{">"}=9</th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td>2.5%</td>
-                    <td>11%</td>
-                    <td>25%</td>
-                    <td>42%</td>
+                    <th scope="row">{"2"}=9</th>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1">2.5%</td>
+                    <td className="px-2 py-1">11%</td>
+                    <td className="px-2 py-1">25%</td>
+                    <td className="px-2 py-1">42%</td>
                 </tr>
                 <tr>
-                    <th scope="row">{">"}=10</th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td>0.4%</td>
-                    <td>3.8%</td>
-                    <td>13%</td>
-                    <td>26%</td>
+                    <th scope="row">{"2"}=10</th>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1">0.4%</td>
+                    <td className="px-2 py-1">3.8%</td>
+                    <td className="px-2 py-1">13%</td>
+                    <td className="px-2 py-1">26%</td>
                 </tr>
                 <tr>
-                    <th scope="row">{">"}=11</th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td>1%</td>
-                    <td>5.2%</td>
-                    <td>14%</td>
+                    <th scope="row">{"2"}=11</th>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1">1%</td>
+                    <td className="px-2 py-1">5.2%</td>
+                    <td className="px-2 py-1">14%</td>
                 </tr>
                 <tr>
-                    <th scope="row">{">"}=12</th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td>0.1%</td>
-                    <td>1.6%</td>
-                    <td>6.4%</td>
+                    <th scope="row">{"2"}=12</th>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1">0.1%</td>
+                    <td className="px-2 py-1">1.6%</td>
+                    <td className="px-2 py-1">6.4%</td>
                 </tr>
                 <tr>
-                    <th scope="row">{">"}=13</th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td>0.4%</td>
-                    <td>2.4%</td>
+                    <th scope="row">{"2"}=13</th>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1">0.4%</td>
+                    <td className="px-2 py-1">2.4%</td>
                 </tr>
                 <tr>
-                    <th scope="row">{">"}=14</th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td>0.05%</td>
-                    <td>0.7%</td>
+                    <th scope="row">{"2"}=14</th>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1">0.05%</td>
+                    <td className="px-2 py-1">0.7%</td>
                 </tr>
                 <tr>
-                    <th scope="row">{">"}=15</th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td>0.1%</td>
+                    <th scope="row">{"2"}=15</th>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1">0.1%</td>
                 </tr>
                 <tr>
-                    <th scope="row">{">"}=16</th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td>0.02%</td>
+                    <th scope="row">{"2"}=16</th>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1"></td>
+                    <td className="px-2 py-1">0.02%</td>
                 </tr>
             </table>
         </div>
