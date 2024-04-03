c35e0790d36bf1bf775ca7148dfd78160b33a45a
Feifei Sun <feifeisun0503@gmail.com>
2024-04-02 20:55:02 -0400

    Change to black background

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,20 +5,20 @@ interface Props {
 const Navbar = ({ title }: Props) => {
     return (
         <>
-            <nav className="bg-white fixed w-full z-20 top-0 start-0 border-b border-gray-200">
-                <div className="h-24 max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <nav className="fixed w-full z-20 top-0 start-0 border-b border-gray-200">
+                <div className="h-16 max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                     <a
                         href="#"
                         className="flex items-center space-x-3 rtl:space-x-reverse md:order-1"
                     >
-                        <span className="self-center text-2xl font-semibold whitespace-nowrap">
+                        <span className="text-white self-center text-2xl font-semibold whitespace-nowrap">
                             {title}
                         </span>
                     </a>
                     <div className="flex space-x-3 md:space-x-0 rtl:space-x-reverse">
                         <button
                             type="button"
-                            className="text-white bg-green-800 hover:bg-yellow-800 focus:ring-4 font-medium rounded-lg text-sm px-4 py-2 text-center"
+                            className="text-white bg-white-800 hover:bg-yellow-800 focus:ring-4 font-medium rounded-lg text-sm px-4 py-2 text-center"
                         >
                             Home
                         </button>
@@ -27,11 +27,11 @@ const Navbar = ({ title }: Props) => {
                         className="items-center justify-between w-full hidden md:flex md:w-auto md:order-2"
                         id="navbar-sticky"
                     >
-                        <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
+                        <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:">
                             <li>
                                 <a
                                     href="#"
-                                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-green-800 md:p-0 "
+                                    className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-yellow-800 md:p-0 "
                                 >
                                     About
                                 </a>
@@ -39,7 +39,7 @@ const Navbar = ({ title }: Props) => {
                             <li>
                                 <a
                                     href="#"
-                                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-green-800 md:p-0"
+                                    className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-yellow-800 md:p-0"
                                 >
                                     Contact
                                 </a>
